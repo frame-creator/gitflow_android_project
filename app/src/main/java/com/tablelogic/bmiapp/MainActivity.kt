dfe7057286dcf5d39e328eb379df16292a812f3a
money <money@moneyui-MacBook-Pro.local>
2021-12-19 19:50:00 +0900

    빈 입력값 핸들링 함수 추가

--- app/src/main/java/com/tablelogic/bmiapp/MainActivity.kt
+++ app/src/main/java/com/tablelogic/bmiapp/MainActivity.kt
@@ -2,10 +2,26 @@ package com.tablelogic.bmiapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val heightEditText: EditText = findViewById(R.id.heightEditText)
+        val weightEditText = findViewById<EditText>(R.id.weightEditText)
+
+        val resultButton = findViewById<Button>(R.id.resultButton)
+
+        resultButton.setOnClickListener {
+
+            if (heightEditText.text.isEmpty() || weightEditText.text.isEmpty()) {
+                Toast.makeText(this, "빈 값이 있습니다.", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+        }
     }
 }
